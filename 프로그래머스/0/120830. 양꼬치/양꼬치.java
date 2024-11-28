79be8c7b22521ca75e04413c8bc56cca3931e6b3
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2024-11-28 10:11:11 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        
-        int service = n / 10;
-        k -= service;
-        answer = (n * 12000) + (k * 2000);
-        return answer;
+        return (n * 12000) + ((k - (n / 10)) * 2000);
     }
 }
