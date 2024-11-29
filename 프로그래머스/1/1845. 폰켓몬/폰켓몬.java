1c04d775b6603cdac9ff9eb034d732714081a184
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2024-11-29 17:15:28 +0900

    [level 1] Title: 폰켓몬, Time: 9.34 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        int count = (int) Arrays.stream(nums)
+        return (int) Arrays.stream(nums)
                 .distinct()
+                .limit(nums.length / 2)
                 .count();
-        int s = nums.length / 2;
-
-        return s > count ? count : s;
     }
 }
