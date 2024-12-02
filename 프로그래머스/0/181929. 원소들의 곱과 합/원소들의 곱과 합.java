f1274a20de1d933970eb53473321e01c51e59765
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2024-12-02 23:26:05 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 1.61 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int product = Arrays.stream(num_list)
+                .reduce((a, b) -> a * b).getAsInt();
+        int sum = Arrays.stream(num_list)
+                .reduce(Integer::sum).getAsInt();
+        return product < Math.pow(sum, 2) ? 1 : 0;
+    }
+}
