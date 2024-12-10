2da08738d0de12eec966393e2281cd11586ea801
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2024-12-10 09:37:16 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = {money/5500, money%5500};
-        return answer;
+        int[] answer = {};
+        
+        return new int[] {money/5500, money%5500};
     }
 }
