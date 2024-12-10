645e6ebabec7adc754b6c47608ed2664a7946dbe
Taehee <134465372+Taeheeeeeee@users.noreply.github.com>
2024-12-10 09:42:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int price) {
-        if(price >= 500000) {
-            price *= 0.8;
-        } else if(price >= 300000){
-            price *= 0.90;
-        } else if(price >= 100000) {
-            price *= 0.95;
-        }
-        return price;
+        return (int) (price >= 500000 ? price*0.8 : price >= 300000 ? price*0.9 : price >= 100000 ? price*0.95 : price);
     }
 }
